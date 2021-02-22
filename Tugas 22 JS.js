876b173e54068c836c6d0798dd21f62c6111985f
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-02-22 20:19:10 +0700

    Tugas 22 JS

---
+++ Tugas 22 JS.js
@@ -0,0 +1,7 @@
+var kalimat = "Saya ingin belajar bersama";
+var ubah = kalimat.split(" ");
+
+ubah.forEach(function(item, index, array) {
+    console.log("Item :", item, "Index ke", index);
+    // console.log("Data dari array di samping = ", array)
+});
